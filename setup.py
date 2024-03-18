8077c2f61ee7a5665e77ec7d876853c28278d31e
Luke Kerbs <luke@Lukes-MBP-3.lan>
2024-03-18 12:32:21 -0700

    changed version

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='forcealign',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=['torch==2.2.1', 'torchaudio==2.2.1', 'pydub==0.25.1', 'g2p-en==2.1.0'],
     author='Luke Kerbs',
     description='A Python library for forced alignment of English text to English audio.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/lukerbs/forcealign',
     keywords=['force align', 'forced alignment', 'audio segmentation', 'audio forced alignment', 'python forced alignment', 'phoneme', 'generate subtitles'],
     classifiers=[
