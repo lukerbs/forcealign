5aa55350b3a687bbb346de4cd4dcf1685bbbcf7b
Luke Kerbs <luke@Lukes-MBP-3.lan>
2024-04-06 21:19:59 -0700

    bugfix

--- forcealign/forcealign.py
+++ forcealign/forcealign.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 import threading 
+import random
 
 import torch
 import torchaudio
@@ -268,8 +269,11 @@ def get_breath_idx(transcript):
 	transcript = alpha_with_punct(transcript).upper().split()
 	idxs = []
 	for i in range(len(transcript)-1):
-		if "," in transcript[i] or "." in transcript[i]:
+		if "," in transcript[i]:
 			idxs.append(i+1)
+		elif "." in transcript[i] and random.choice([True, False, False]):
+			idxs.append(i+1)
+
 	return idxs
 
 def alpha_with_punct(text):
